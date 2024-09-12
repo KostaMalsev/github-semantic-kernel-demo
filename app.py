af3b7920f6113e67f49faacd03dbc7e9ee8a8f98
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2024-09-12 15:44:55 +0300

    update the api keys handling

--- app.py
+++ app.py
@@ -235,13 +235,13 @@ async def setup_kernel():
 
     if os.getenv('GLOBAL_LLM_SERVICE') != "AzureOpenAI":
         raise ValueError("This script is configured to use Azure OpenAI. Please check your .env file.")
-    
 
-    azure_api_key = os.getenv('AZURE_OPENAI_API_KEY') or read_secret('AZURE_OPENAI_API_KEY')
-    github_api_key = os.getenv('GITHUB_TOKEN_GEN_AI') or read_secret('GITHUB_TOKEN_GEN_AI')
-    
-    os.environ['AZURE_OPENAI_API_KEY'] = azure_api_key
-    os.environ['GITHUB_TOKEN_GEN_AI'] = github_api_key
+    if( (not os.getenv('AZURE_OPENAI_API_KEY')) or (not os.getenv('AZURE_OPENAI_API_KEY')) ):
+        azure_api_key = os.getenv('AZURE_OPENAI_API_KEY') or read_secret('AZURE_OPENAI_API_KEY')
+        github_api_key = os.getenv('GITHUB_TOKEN_GEN_AI') or read_secret('GITHUB_TOKEN_GEN_AI')
+        
+        os.environ['AZURE_OPENAI_API_KEY'] = azure_api_key
+        os.environ['GITHUB_TOKEN_GEN_AI'] = github_api_key
     
     service_id = "function_calling"
     
